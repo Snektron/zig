10ad2a2d45508915e0169b7f6c9045bce874ce2c
Robin Voetter <robin@voetter.nl>
2023-09-17 13:30:18 +0200

    fix printing pointers with undefined address

--- src/TypedValue.zig
+++ src/TypedValue.zig
@@ -255,10 +255,13 @@ pub fn print(
             },
             .ptr => |ptr| {
                 if (ptr.addr == .int) {
-                    const i = ip.indexToKey(ptr.addr.int).int;
-                    switch (i.storage) {
-                        inline else => |addr| return writer.print("{x:0>8}", .{addr}),
-                    }
+                    return switch (ip.indexToKey(ptr.addr.int)) {
+                        .int => |i| switch (i.storage) {
+                            inline else => |addr| writer.print("{x:0>8}", .{addr}),
+                        },
+                        .undef => return writer.writeAll("undefined"),
+                        else => unreachable,
+                    };
                 }
 
                 const ptr_ty = ip.indexToKey(ty.toIntern()).ptr_type;
